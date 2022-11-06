d35e44eafccc39d136354fdc7147091f68e3c07f
bảo hoàng <nglehoangbao@gmail.com>
2022-11-06 21:02:44 +0700

    T1

---
+++ WeatherForecast.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace API_hienchanel
+{
+    public class WeatherForecast
+    {
+        // T1
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
